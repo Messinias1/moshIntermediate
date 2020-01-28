5488be788ace59b6c2d112c3cf6a0caf185a48c3
Carl Kakisis <Messinias1@gmail.com>
2020-01-28 13:00:01 -0500

    Fields video and demo complete

--- moshIntermediate/Program.cs
+++ moshIntermediate/Program.cs
@@ -1,58 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace moshIntermediate
 {
-    public class Calculator
-    {
-        public int Add(params int[] numbers)
-        {
-            var sum = 0;
-            foreach(var number in numbers)
-            {
-                sum += number;
-            }
-            return sum;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            //int.Parse("abc");
-            int number;
-            var result = int.TryParse("Abc", out number);
-            if(result)
-                Console.WriteLine(number);
-            else
-                Console.WriteLine("Conversion Failed");
-        }
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
 
-        static void useParams()
-        {
-            var calculator = new Calculator();
-            Console.WriteLine(calculator.Add(1, 2, 3, 4, 5));
-        }
+            customer.Promote();
 
-        static void usePoints()
-        {
-            try
-            {
-                var point = new Point(10, 20);
-                Console.WriteLine("Original Point {0}, {1}: ", point.X, point.Y);
-                point.Move(null);
-                Console.WriteLine("Point is at {0}, {1}", point.X, point.Y);
-
-                point.Move(100, 200);
-                Console.WriteLine("Point moved to {0}, {1}", point.X, point.Y);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("an unexpected occured");
-            }
+            Console.WriteLine(customer.Orders.Count());
         }
     }
 }
