500afc843401797961a915adde20f6b58ae94aca
Carl Kakisis <Messinias1@gmail.com>
2020-01-28 21:48:23 -0500

    Properties demo

--- moshIntermediate/Program.cs
+++ moshIntermediate/Program.cs
@@ -5,25 +5,12 @@ using System.Threading.Tasks;
 
 namespace moshIntermediate
 {
-    public class Person
-    {
-        private DateTime _birthdate;
-        public void SetBirthdate(DateTime birthdate)
-        {
-            _birthdate = birthdate;
-        }
-        public DateTime GetBirthdate()
-        {
-            return _birthdate;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.SetBirthdate(new DateTime(1982, 1, 1));
-            Console.WriteLine(person.GetBirthdate());
+            var person = new Person(new DateTime(1994, 12, 6));
+            Console.WriteLine(person.Age);
         }
     }
 }
